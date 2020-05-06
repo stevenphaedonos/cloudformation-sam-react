22e93865659adce1f32008918b8ddbbb53497ef7
Steven Phaedonos <steven@phaedonos.com>
2020-05-06 20:11:28 +1000

    Extract user claims from validated token

--- backend/src/decorators.py
+++ backend/src/decorators.py
@@ -1,13 +1,21 @@
 import functools
 import json
+from base64 import b64decode
+
+# It is assumed that the token has already been validated by the API Gateway 
+# Cognito authorizer
+def get_claims(event):
+    auth_token = event["headers"]["Authorization"]
+    claims = auth_token.split(".")[1]
+    decoded_claims = b64decode(claims)
+    return json.loads(decoded_claims)
 
 
 def is_admin(func):
     @functools.wraps(func)
     def wrapper(event, context):
-        if "admin" in event["requestContext"]["authorizer"].get(
-            "cognito:groups", ""
-        ).split(","):
+        groups = get_claims(event).get("cognito:groups", [])
+        if "admin" in groups:
             return func(event, context)
 
         return {
